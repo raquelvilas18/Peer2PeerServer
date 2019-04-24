c2059ea9859aff9208eba1c77db1775dd0688b2f
raquel <raquel@MSI>
2019-04-24 20:21:02 +0200

    holi

--- Peer2PeerServer/src/peer2peer/ClientInterface.java
+++ Peer2PeerServer/src/peer2peer/ClientInterface.java
@@ -11,14 +11,14 @@ import java.util.*;
  * @author raquel
  */
 public interface ClientInterface extends java.rmi.Remote{
-    public void nuevoAmigoConectado(ClientInterface amigoConectado) 
-      throws java.rmi.RemoteException;
-    public void nuevoAmigoDesconectado(ClientInterface amigoConectado) 
-      throws java.rmi.RemoteException;
-      
-    public Vector getAmigos() throws java.rmi.RemoteException;
-    public void setAmigos(Vector amigos) throws java.rmi.RemoteException;
-    public String getNombre()throws java.rmi.RemoteException;
-
-    
+    public void nuevoAmigoConectado(ClientInterface amigoConectado) throws java.rmi.RemoteException;
+    public void nuevoAmigoDesconectado(ClientInterface amigoDesconectado) throws java.rmi.RemoteException; 
+    public HashMap<String, ClientInterface> getAmigos() throws java.rmi.RemoteException;
+    public void setAmigos(ArrayList<String> amigos) throws java.rmi.RemoteException;
+    public HashMap<String, ClientInterface> getAmigosConectados();
+    public HashMap<String, ClientInterface> getPeticionesAmistad();
+    public void setAmigosConectados(HashMap<String, ClientInterface> amigosConectados);
+    public void setPeticionesAmistad(ArrayList<String> peticionesAmistad);
+    public String getNombre();
+    public void setNombre(String nombre);
 }
--- Peer2PeerServer/src/peer2peer/ServerImpl.java
+++ Peer2PeerServer/src/peer2peer/ServerImpl.java
@@ -10,6 +10,7 @@ import java.rmi.server.UnicastRemoteObject;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Properties;
 import java.util.Vector;
@@ -22,7 +23,7 @@ import java.util.logging.Logger;
  */
 public class ServerImpl extends UnicastRemoteObject implements ServerInterface {
 
-    private HashMap<String,ClientInterface> clientesActivos;
+    private HashMap<String, ClientInterface> clientesActivos;
     private java.sql.Connection conexion;
 
     public ServerImpl() throws RemoteException {
@@ -32,21 +33,23 @@ public class ServerImpl extends UnicastRemoteObject implements ServerInterface {
         //
     }
 
-    public void iniciarSesion(ClientInterface ClientObject) throws java.rmi.RemoteException {
+    public void iniciarSesion(ClientInterface usuario) throws java.rmi.RemoteException {
         Vector amigosConectados = new Vector();
 
         // Añadir el cliente que inicio sesion a la lista de clientes activos
-        if (!(clientesActivos.containsKey(ClientObject.getNombre()))) {
-            clientesActivos.put(ClientObject.getNombre(),ClientObject);
-            System.out.println(ClientObject.getNombre() + " ha iniciado sesion");
+        if (!(clientesActivos.containsKey(usuario.getNombre()))) {
+            clientesActivos.put(usuario.getNombre(), usuario);
+            System.out.println(usuario.getNombre() + " ha iniciado sesion");
         }
         //OBTENER LISTA DE AMIGOS DEL USUARIO+ .nuevoAmigoConectado()
-        this.
-
+        ArrayList<String> amigos = obtenerAmigos(usuario.getNombre());
+        usuario.setAmigos(amigos);
+        usuario.setAmigosConectados(obtenerNotificarAmigosConectados(amigos, usuario));
+        usuario.setPeticionesAmistad(obtenerPeticiones(usuario.getNombre()));
     }
 
     public void cerrarSesion(ClientInterface ClientObject) throws java.rmi.RemoteException {
-        Vector amigos = ClientObject.getAmigos();
+        //Vector amigos = ClientObject.getAmigos();
     }
 
     public ClientInterface buscarPersona(String nombre) {
@@ -73,27 +76,61 @@ public class ServerImpl extends UnicastRemoteObject implements ServerInterface {
             System.out.println("Imposible conectar con la Base de datos");
         }
     }
-    
+
     //Consulta en la base de datos de los amigos de un usuario dado
-    private HashMap<String, ClientInterface> obtenerAmigos(String usuario) {
-        HashMap<String, ClientInterface> amigos = new HashMap<String, ClientInterface>();
+    private ArrayList<String> obtenerAmigos(String usuario) {
+        ArrayList<String> amigos = new ArrayList<String>();
         String amigo;
         PreparedStatement stm;
         try {
             stm = conexion.prepareStatement("SELECT amigo FROM amigos WHERE usuario=?");
             stm.setString(1, usuario);
             ResultSet rs = stm.executeQuery();
-            while(rs.next()){
-                amigo = rs.getString("amigo");
-                if(this.clientesActivos.containsKey(amigo)){
-                    amigos.put(amigo, clientesActivos.get(amigo));
-                }
+            while (rs.next()) {
+                amigos.add(rs.getString("amigo"));
             }
             return amigos;
         } catch (SQLException ex) {
             Logger.getLogger(ServerImpl.class.getName()).log(Level.SEVERE, null, ex);
             return null;
-        } 
+        }
+    }
+
+    //Recorre la lista de amigos del cliente; devuelve referencias a todos los conectados y a estos les notifica que se ha conectado
+    private HashMap<String, ClientInterface> obtenerNotificarAmigosConectados(ArrayList<String> amigosTotales, ClientInterface usuario) {
+        HashMap<String, ClientInterface> amigosConectados = new HashMap<>();
+        try {
+            for (String amigo : amigosTotales) {
+                if (this.clientesActivos.containsKey(amigo)) {
+                    amigosConectados.put(amigo, clientesActivos.get(amigo));
+                    clientesActivos.get(amigo).nuevoAmigoConectado(usuario);
+
+                }
+            }
+            return amigosConectados;
+        } catch (RemoteException ex) {
+            Logger.getLogger(ServerImpl.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+    }
+    
+    //Consulta en la BD las peticiones de amistad que tiene pendientes el usuario
+    private ArrayList<String> obtenerPeticiones(String usuario){
+         ArrayList<String> peticiones = new ArrayList<String>();
+        String emisor;
+        PreparedStatement stm;
+        try {
+            stm = conexion.prepareStatement("SELECT emisor FROM peticiones WHERE receptor=?");
+            stm.setString(1, usuario);
+            ResultSet rs = stm.executeQuery();
+            while (rs.next()) {
+                peticiones.add(rs.getString("amigo"));
+            }
+            return peticiones;
+        } catch (SQLException ex) {
+            Logger.getLogger(ServerImpl.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
     }
 
 }
